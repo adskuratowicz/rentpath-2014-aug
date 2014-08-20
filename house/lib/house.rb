d5b423926ff4f08cf1b292db8c454131a43655a6
Martin Streicher <martin.streicher@gmail.com>
2014-08-20 15:03:10 -0400

    Song modified.

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,6 +1,28 @@
-# Feel free to delete the instructions once you get going
-puts instructions(__FILE__)
-
 class House
+  DATA = [
+    'the house that Jack built',
+    'the malt that lay in',
+    'the rat that ate',
+    'the cat that killed',
+    'the dog that worried',
+    'the cow with the crumpled horn that tossed',
+    'the maiden all forlorn that milked',
+    'the man all tattered and torn that kissed',
+    'the priest all shaven and shorn that married',
+    'the rooster that crowed in the morn that woke',
+    'the farmer sowing his corn that kept',
+    'the horse and the hound and the horn that belonged to'
+  ].freeze
 
+  def recite
+   (1..12).to_a.map { |n| line n }.join("\n")
+  end
+
+  def line(number)
+    'This is ' + segments(number).join(' ') + ".\n"
+  end
+
+  def segments(number)
+    DATA[0..number-1].reverse
+  end
 end
