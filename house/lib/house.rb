a63c63ad7ee82b838bb16f3e070c6fbc4ded19fd
TJ Stankus <tjstankus@gmail.com>
2014-08-25 10:10:25 -0400

    Extract phrase ordering

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,4 +1,22 @@
+class DefaultOrderer
+  def order(items)
+    items
+  end
+end
+
+class RandomOrderer
+  def order(items)
+    items.shuffle
+  end
+end
+
+
 class House
+  attr_reader :lines
+
+  def initialize(orderer = DefaultOrderer.new)
+    @lines = orderer.order(phrases)
+  end
 
   def recite
     1.upto(12).collect {|i| line(i)}.join("\n")
@@ -9,17 +27,23 @@ class House
   end
 
   def phrase(num)
-    [ "the horse and the hound and the horn that belonged to",
-      "the farmer sowing his corn that kept",
-      "the rooster that crowed in the morn that woke",
-      "the priest all shaven and shorn that married",
-      "the man all tattered and torn that kissed",
-      "the maiden all forlorn that milked",
-      "the cow with the crumpled horn that tossed",
-      "the dog that worried",
-      "the cat that killed",
-      "the rat that ate",
-      "the malt that lay in",
-      "the house that Jack built"].last(num).join(" ")
+    lines.last(num).join(" ")
+  end
+
+  def phrases
+    ["the horse and the hound and the horn that belonged to",
+     "the farmer sowing his corn that kept",
+     "the rooster that crowed in the morn that woke",
+     "the priest all shaven and shorn that married",
+     "the man all tattered and torn that kissed",
+     "the maiden all forlorn that milked",
+     "the cow with the crumpled horn that tossed",
+     "the dog that worried",
+     "the cat that killed",
+     "the rat that ate",
+     "the malt that lay in",
+     "the house that Jack built"]
   end
 end
+
+puts House.new(RandomOrderer.new).recite
